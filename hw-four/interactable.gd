cc5e0357fa241e5de03d2c1c58780cf4562505f6
musical-nomad <sosjade.sh.jw@gmail.com>
2026-04-08 14:44:14 -0400

    the things

---
+++ hw-four/interactable.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+func interact():
+	var collected = false
+	var talked_to = false
+	var read = false
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass

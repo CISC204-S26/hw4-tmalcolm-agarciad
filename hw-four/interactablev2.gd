6ba2d98f0cb81d50cd66917129b7ca6f33c64017
musical-nomad <sosjade.sh.jw@gmail.com>
2026-04-20 14:05:50 -0400

    ;0

---
+++ hw-four/interactablev2.gd
@@ -0,0 +1,15 @@
+class_name interactablev2 extends Area2D
+
+@export var test = "This is a test"
+
+
+func interact():
+	push_warning("This interactable has no code yet bro")
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
